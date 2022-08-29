f89927d90ed23760aace5e3c06d1cc8f85819ea4
Alireza Mirhosseini <shapourdisco78@gmail.com>
2022-08-29 21:27:05 +0430

    Prop Drilling

---
+++ Prop Drilling React/src/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id: 1,
+        name: "Alireza",
+    },
+    {
+        id: 2,
+        name: "Shapour",
+    },
+    {
+        id: 3,
+        name: "Gomnam",
+    }
+]
