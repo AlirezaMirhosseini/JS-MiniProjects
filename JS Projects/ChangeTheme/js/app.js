0b1674a526a61609eeb7a94c8e03855ad312b80b
Alireza Mirhosseini <shapourdisco78@gmail.com>
2023-02-08 21:32:15 +0330

    Change Theme 2

---
+++ JS Projects/ChangeTheme/js/app.js
@@ -0,0 +1,23 @@
+const switchElement = document.querySelector('.switch')
+
+switchElement.addEventListener('click', function () {
+  document.body.classList.toggle('dark')
+
+  if (document.body.className.includes('dark')) {
+    localStorage.setItem('theme', 'dark')
+  } else {
+    localStorage.setItem('theme', 'light')
+  }
+
+  // console.log(document.body.className.includes('dark'));  // Boolean
+})
+
+
+window.onload = function () {
+  let localStorageTheme = localStorage.getItem('theme')
+
+  if (localStorageTheme === 'dark') {
+    document.body.classList.add('dark')
+  }
+
+}
